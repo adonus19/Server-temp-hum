a61ae9640fd03906e3652ef0012210230690d2cf
adonus19 <daniel.j.pogue@gmail.com>
2024-12-16 13:04:54 -0500

    first commit

---
+++ stepper_web_server.ino
@@ -0,0 +1,113 @@
+#include <Arduino.h>
+#include <WiFi.h>
+#include <AsyncTCP.h>
+#include <ESPAsyncWebServer.h>
+#include <Stepper.h>
+#include "LittleFS.h"
+
+// Stepper Motor Settings
+const int stepsPerRevolution = 2048;
+#define IN1 19
+#define IN2 18
+#define IN3 5
+#define IN4 17
+Stepper myStepper(stepsPerRevolution, IN1, IN3, IN2, IN4);
+
+// Replace with your network credentials
+const char* ssid = "Rogue_Pogue";
+const char* password = "Rogue!2023";
+
+// Create AsyncWebServer object on port 80
+AsyncWebServer server(80);
+
+// Search for parameters in HTTP POST request
+const char* PARAM_INPUT_1 = "direction";
+const char* PARAM_INPUT_2 = "steps";
+
+// Variables to save values from HTML form
+String direction;
+String steps;
+
+// Variable to detect whether a new request occurred
+bool newRequest = false;
+
+// Initialize LittleFS
+void initLittleFS() {
+  if (!LittleFS.begin(true)) {
+    Serial.println("An error has occurred while mounting LittleFS");
+  }
+  else {
+  Serial.println("LittleFS mounted successfully");
+  }
+}
+
+// Initialize WiFi
+void initWiFi() {
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(ssid, password);
+  Serial.print("Connecting to WiFi ..");
+  while (WiFi.status() != WL_CONNECTED) {
+    Serial.print('.');
+    delay(1000);
+  }
+  Serial.println(WiFi.localIP());
+}
+
+
+void setup() {
+  Serial.begin(115200);
+
+  initLittleFS();
+  initWiFi();
+
+  myStepper.setSpeed(5);
+
+  // Web Server Root URL
+  server.on("/", HTTP_GET, [](AsyncWebServerRequest *request){
+    request->send(LittleFS, "/index.html", "text/html");
+  });
+
+  // Serve the CSS
+  server.serveStatic("/", LittleFS, "/");
+  
+  // Handle request (form)
+  server.on("/", HTTP_POST, [](AsyncWebServerRequest *request) {
+    int params = request->params();
+    for(int i=0;i<params;i++){
+      const AsyncWebParameter* p = request->getParam(i);
+      if(p->isPost()){
+        // HTTP POST input1 value (direction)
+        if (p->name() == PARAM_INPUT_1) {
+          direction = p->value().c_str();
+          Serial.print("Direction set to: ");
+          Serial.println(direction);
+        }
+        // HTTP POST input2 value (steps)
+        if (p->name() == PARAM_INPUT_2) {
+          steps = p->value().c_str();
+          Serial.print("Number of steps set to: ");
+          Serial.println(steps);
+        }
+      }
+    }
+    request->send(LittleFS, "/index.html", "text/html");
+    newRequest = true;
+  });
+
+  server.begin();
+}
+
+void loop() {
+  // Check if there was a new request and move the stepper accordingly
+  if (newRequest){
+    if (direction == "CW"){
+      // Spin the stepper clockwise direction
+      myStepper.step(steps.toInt());
+    }
+    else{
+      // Spin the stepper counterclockwise direction
+      myStepper.step(-steps.toInt());
+    }
+    newRequest = false;
+  }
+}
